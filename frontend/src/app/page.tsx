5c5f659d8fca46c532c1a022cccf4564cc3c9c23
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-03 18:04:53 +0800

    toppage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,22 +10,10 @@ import "./page.module.css";
 export default function Home() {
   const router = useRouter();
 
-  const onSubmit = async (email: string, password: string) => {
-    console.log(`Email: ${email}, Password: ${password}`);
-    const res: AxiosResponse = await axios.post("/login", { email, password });
-    console.log("res", res);
-    if (res.status === 200) {
-      localStorage.setItem("session_token", res.data.sessionToken);
-      router.push(`/user/${res.data.id}`);
-    }
-  };
-
   return (
     <main>
       <h1>Hello super-metro</h1>
-      <h2>This is a login form </h2>
-      <LoginForm onSubmit={onSubmit} />
-      <Link href={"/signup"}>still not have account??</Link>
+      <h2>This is top page </h2>
     </main>
   );
 }
