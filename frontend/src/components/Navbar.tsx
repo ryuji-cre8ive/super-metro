a4d1d058a816df9cb8602981c5a2c55d9dc4f75d
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-07 16:43:57 +0800

    navigation barをカスタムしました

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,13 +6,49 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
+import Avatar from "@mui/material/Avatar";
+import MenuItem from "@mui/material/MenuItem";
+import PersonIcon from "@mui/icons-material/Person";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import { useAuth } from "@/app/userContext";
 
+import { useRouter } from "next/navigation";
+
 export default function ButtonAppBar() {
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+  const router = useRouter();
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   const { user } = useAuth();
+  let settings: { label: string; link: string }[] = [];
+  if (user) {
+    settings = [
+      {
+        label: "top up",
+        link: `/user/${user.id}/top-up`,
+      },
+      {
+        label: "main page",
+        link: `/user/${user.id}`,
+      },
+      {
+        label: "logout",
+        link: `/user/${user.id}/logout`,
+      },
+    ];
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -32,7 +68,45 @@ export default function ButtonAppBar() {
           {user ? (
             <>
               <Typography variant="subtitle1" component="div">
-                {user.userName}
+                <Box sx={{ flexGrow: 0 }}>
+                  <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                      <Avatar>
+                        <PersonIcon />
+                      </Avatar>
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    sx={{ mt: "45px" }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                  >
+                    {settings.map((setting) => (
+                      <MenuItem
+                        key={setting.label}
+                        onClick={() => {
+                          handleCloseUserMenu();
+                          router.push(setting.link);
+                        }}
+                      >
+                        <Typography textAlign="center">
+                          {setting.label}
+                        </Typography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </Box>
               </Typography>
               <Button color="inherit" href={"/user/" + user.id + "/signout"}>
                 Logout
