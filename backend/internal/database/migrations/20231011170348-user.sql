bb5a23c20b1b8d249ea0b6693a60adc5e9a6d88e
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-08 23:41:53 +0800

    DBスキーマ修正

--- backend/internal/database/migrations/20231011170348-user.sql
+++ backend/internal/database/migrations/20231011170348-user.sql
@@ -5,7 +5,11 @@ CREATE TABLE "users" (
     name text,
     email text,
     password text,
-    session_token text
+    session_token text,
+    valance integer,
+    created_at timestamp,
+    updated_at timestamp,
+    deleted_at timestamp
 );
 -- +migrate Down
-DROP TABLE users;
+DROP TABLE "users";
