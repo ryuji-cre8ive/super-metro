8ca4cc6aba2cd3e8699928c3342d490aa6f59b70
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-08 23:44:30 +0800

    loginページ改修

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -102,7 +102,7 @@ const LoginForm = ({ onSubmit }: LoginFormProps) => {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/signup" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
