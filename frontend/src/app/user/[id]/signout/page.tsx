71c768274e53cc60d67c57b029edc40d09b4314c
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-09 11:17:47 +0800

    signoutページ改修

--- frontend/src/app/user/[id]/signout/page.tsx
+++ frontend/src/app/user/[id]/signout/page.tsx
@@ -2,24 +2,41 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "@/api/axiosConfig";
-import Button from "@mui/material/Button";
+
 import { useAuth } from "@/app/userContext";
+import { Button, Typography, Box, Paper } from "@mui/material";
 
 export default function SignOutPage() {
   const { logout } = useAuth();
   const router = useRouter();
   const onSignOut = async () => {
-    if (!confirm("are you sure you want to sign out?")) {
-      return;
-    }
     window.localStorage.removeItem("session_token");
     logout();
     await axios.post("/logout");
     router.push("/");
   };
   return (
-    <>
-      <Button onClick={onSignOut}>Logout</Button>
-    </>
+    <Box sx={{ display: "flex", alignItems: "center", height: "100vh" }}>
+      <Paper
+        elevation={3}
+        sx={{ p: 4, borderRadius: 2, maxWidth: 400, mx: "auto" }}
+      >
+        <Box sx={{ textAlign: "center" }}>
+          <Typography variant="h5" component="h1" sx={{ mb: 2 }}>
+            Are You Sure You Want To Sign Out?
+          </Typography>
+
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            sx={{ px: 4 }}
+            onClick={onSignOut}
+          >
+            Go SignOut
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
   );
 }
