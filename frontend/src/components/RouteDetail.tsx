f4578db1baf099dc8a9638959320dd4ac5bf86ad
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-09 11:21:13 +0800

    flex-boxの割合変更

--- frontend/src/components/RouteDetail.tsx
+++ frontend/src/components/RouteDetail.tsx
@@ -5,14 +5,21 @@ const RouteDetails = ({ route }: { route: google.maps.DirectionsRoute }) => {
   const legs = route.legs;
   return (
     <Card style={{ width: "50%", display: "flex", padding: "30px" }}>
-      <section>
+      <section style={{ flexGrow: "7" }}>
         <Typography>
           {legs[0].departure_time?.text} → {legs[0].arrival_time?.text}
         </Typography>
         <Typography>OneWay</Typography>
         <Typography>Steps: {legs[0].steps.length}</Typography>
       </section>
-      <div style={{ margin: "0 auto", display: "flex", alignItems: "center" }}>
+      <div
+        style={{
+          margin: "0 auto",
+          display: "flex",
+          alignItems: "center",
+          flexGrow: "3",
+        }}
+      >
         <Typography>{legs[0].duration?.text}</Typography>
       </div>
 
