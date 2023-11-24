42142cb7b644e98347ca5e0fbe00a9361858414f
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-24 16:30:08 +0800

    paymentを追加しました

--- backend/internal/database/migrations/20231011170348-user.sql
+++ backend/internal/database/migrations/20231011170348-user.sql
@@ -11,5 +11,17 @@ CREATE TABLE "users" (
     updated_at timestamp,
     deleted_at timestamp
 );
+
+CREATE TABLE payment (
+    id text PRIMARY KEY,
+    user_id text,
+    card_number text,
+    expiry_date text,
+    cvv text,
+    created_at timestamp,
+    updated_at timestamp,
+    deleted_at timestamp
+);
 -- +migrate Down
 DROP TABLE "users";
+DROP TABLE "payment";
