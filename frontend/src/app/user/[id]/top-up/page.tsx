dce192a885ce7a6d1557482ddfa0a4d5bd232503
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-30 11:49:41 +0800

    Add card component and update top-up functionality

--- frontend/src/app/user/[id]/top-up/page.tsx
+++ frontend/src/app/user/[id]/top-up/page.tsx
@@ -1,22 +1,76 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Typography } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import SendIcon from "@mui/icons-material/Send";
 import Image from "next/image";
 import CustomSnackBar, { Severity } from "@/components/SnackBar";
 import EWalletPic from "../../../../../public/e-wallet.png";
-import { SnackBarTextErr, SnackBarTextSuccess } from "./text";
+import {
+  SnackBarTextErr,
+  SnackBarTextSuccess,
+  SnackBarTextInputErr,
+  SnackBarTextInputSuccess,
+} from "./text";
 import { useAuth } from "@/app/userContext";
 import axios from "@/api/axiosConfig";
 import { AxiosResponse } from "axios";
+import Card from "./components/Card";
 
 const TopUp = () => {
   const { user, topUp } = useAuth();
   const [amount, setAmount] = useState<number | null>(null);
   const [errOpen, setErrOpen] = useState<boolean>(false);
   const [successOpen, setSuccessOpen] = useState<boolean>(false);
+  const [text, setText] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
+  const [cardNumber, setCardNumber] = useState<string>("");
+
+  useEffect(() => {
+    if (!user) return;
+    getCardNumber(user.id);
+  }, [user]);
+
+  const getCardNumber = async (userId: string) => {
+    axios.get(`/credit-card/${userId}`).then((res) => {
+      res.data.CardNumber && setCardNumber(res.data.CardNumber);
+    });
+  };
+
+  const onChangeCardNumber = (cardNumber: string) => {
+    setCardNumber(cardNumber);
+  };
+
+  const onSubmit = async (
+    userId: string,
+    cardNumber: string,
+    expiryDate: string,
+    cvv: string
+  ) => {
+    const params = {
+      userId,
+      cardNumber,
+      expiryDate,
+      cvv,
+    };
+    if (!userId || !cardNumber || !expiryDate || !cvv) {
+      setText(SnackBarTextInputErr);
+      return setErrOpen(true);
+    }
+    try {
+      await axios.post("/credit-card/add", params).then((res) => {
+        if (res.status !== 200) {
+          setText(SnackBarTextInputErr);
+          setErrOpen(true);
+        }
+        setText(SnackBarTextInputSuccess);
+        setSuccessOpen(true);
+      });
+    } catch (e) {
+      setText(SnackBarTextInputErr);
+      setErrOpen(true);
+    }
+  };
 
   const isNumber = (value: any) => {
     var pattern = /^[0-9]*$/;
@@ -36,6 +90,7 @@ const TopUp = () => {
   const handleTopUp = () => {
     if (!amount) return;
     if (!isNumber(amount)) {
+      setText(SnackBarTextErr);
       return setErrOpen(true);
     }
     if (!user) return;
@@ -47,6 +102,7 @@ const TopUp = () => {
           if (res.status !== 200) return;
           topUp(user, Number(amount));
           window.localStorage.setItem("session_token", res.data.sessionToken);
+          setText(SnackBarTextSuccess);
           setSuccessOpen(true);
           setAmount(null);
           setLoading(false);
@@ -67,6 +123,13 @@ const TopUp = () => {
       }}
     >
       <Image src={EWalletPic} alt="e-wallets" loading="lazy" width={300} />
+      <Card
+        onSubmit={onSubmit}
+        currentCardNumber={cardNumber}
+        userId={user ? user.id : ""}
+        onChangeCardNumber={onChangeCardNumber}
+      />
+      {}
       <Typography variant="subtitle1">
         Valance: {user && user.valance}
       </Typography>
@@ -85,13 +148,13 @@ const TopUp = () => {
         open={errOpen}
         setOpen={setErrOpen}
         severity={Severity.ERROR}
-        text={SnackBarTextErr}
+        text={text}
       />
       <CustomSnackBar
         open={successOpen}
         setOpen={setSuccessOpen}
         severity={Severity.SUCCESS}
-        text={SnackBarTextSuccess}
+        text={text}
       />
     </main>
   );
