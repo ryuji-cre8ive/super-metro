4f06d37eaf283278b89526ae227f03a00002949a
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-30 11:50:13 +0800

    Add card number error and success messages

--- frontend/src/app/user/[id]/top-up/text.ts
+++ frontend/src/app/user/[id]/top-up/text.ts
@@ -1,2 +1,5 @@
 export const SnackBarTextErr = "top-up failed! please enter valid amount";
 export const SnackBarTextSuccess = "top-up success!";
+
+export const SnackBarTextInputErr = "something went wrong with card number";
+export const SnackBarTextInputSuccess = "card is added successfully!";
