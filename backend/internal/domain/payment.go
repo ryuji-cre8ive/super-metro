ec3278581099f5e1d53c4e82a76c59d6da49018d
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-12-02 01:34:28 +0800

    Refactor Payment struct to Transaction struct

--- backend/internal/domain/payment.go
+++ backend/internal/domain/payment.go
@@ -4,13 +4,13 @@ import (
 	"time"
 )
 
-type Payment struct {
-	ID         string    `db:"id"`
-	UserID     string    `db:"userId"`
-	CardNumber string    `db:"cardNumber"`
-	ExpiryDate string    `db:"expiryDate"`
-	CVV        string    `db:"cvv"`
-	CreatedAt  time.Time `db:"createdAt"`
-	UpdatedAt  time.Time `db:"updatedAt"`
-	DeletedAt  *time.Time `db:"deletedAt"`
+type Transaction struct {
+	ID              string     `db:"id" json:"id"`
+	UserID          string     `db:"userId" json:"userId"`
+	PaymentID       string     `db:"paymentId" json:"paymentId"`
+	Amount          int        `db:"amount" json:"amount"`
+	TransactionType string     `db:"transactionType" json:"transactionType"`
+	CreatedAt       time.Time  `db:"createdAt" json:"createdAt"`
+	UpdatedAt       time.Time  `db:"updatedAt" json:"updatedAt"`
+	DeletedAt       *time.Time `db:"deletedAt" json:"deletedAt"`
 }
