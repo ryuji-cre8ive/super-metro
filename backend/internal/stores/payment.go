1c4cd27d55a3857b622b8913adc37ca8abc853dd
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-12-01 10:59:39 +0800

    Add encryption for payment information

--- backend/internal/stores/payment.go
+++ backend/internal/stores/payment.go
@@ -1,11 +1,13 @@
 package stores
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 
 	"github.com/ryuji-cre8ive/super-metro/internal/domain"
+	"github.com/ryuji-cre8ive/super-metro/internal/utils"
 	"gorm.io/gorm"
 )
 
@@ -22,12 +24,29 @@ type (
 )
 
 func (s *paymentStore) Add(userId string, cardNumber string, expiryDate string, cvv string) error {
+	key := []byte(userId)[:32] // 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256
+
+	encryptedCardNumber, err := utils.Encrypt([]byte(cardNumber), key)
+	if err != nil {
+		return err
+	}
+
+	encryptedExpiryDate, err := utils.Encrypt([]byte(expiryDate), key)
+	if err != nil {
+		return err
+	}
+
+	encryptedCVV, err := utils.Encrypt([]byte(cvv), key)
+	if err != nil {
+		return err
+	}
+
 	return s.DB.Create(&domain.Payment{
 		ID:         uuid.Must(uuid.NewRandom()).String(),
 		UserID:     userId,
-		CardNumber: cardNumber,
-		ExpiryDate: expiryDate,
-		CVV:        cvv,
+		CardNumber: string(encryptedCardNumber),
+		ExpiryDate: string(encryptedExpiryDate),
+		CVV:        string(encryptedCVV),
 		CreatedAt:  time.Now(),
 		UpdatedAt:  time.Now(),
 	}).Error
@@ -40,5 +59,29 @@ func (s *paymentStore) Delete(userId string) error {
 func (s *paymentStore) GetCreditCard(userId string) (*domain.Payment, error) {
 	var payment *domain.Payment
 	result := s.DB.Where("user_id = ? AND deleted_at IS NULL", userId).Find(&payment)
+	fmt.Println("result", payment)
+	if payment.ID == "" {
+		return nil, nil
+	}
+	key := []byte(userId)[:32] // AES-256を選択するために32バイトを使用します。userIDはuuidで、36バイトですが、最初の32バイトだけを使用します。
+
+	decryptedCardNumber, err := utils.Decrypt(payment.CardNumber, key)
+	if err != nil {
+		return nil, err
+	}
+
+	decryptedExpiryDate, err := utils.Decrypt(payment.ExpiryDate, key)
+	if err != nil {
+		return nil, err
+	}
+
+	decryptedCVV, err := utils.Decrypt(payment.CVV, key)
+	if err != nil {
+		return nil, err
+	}
+
+	payment.CardNumber = string(decryptedCardNumber)
+	payment.ExpiryDate = string(decryptedExpiryDate)
+	payment.CVV = string(decryptedCVV)
 	return payment, result.Error
 }
--- backend/internal/utils/encrypt.go
+++ backend/internal/utils/encrypt.go
@@ -1,6 +1,15 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 // 暗号化 (hash)
 func PasswordEncrypt(password string) (string, error) {
@@ -11,4 +20,48 @@ func PasswordEncrypt(password string) (string, error) {
 // 暗号化パスワードと比較
 func CheckHashPassword(hashPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte(password))
+}
+
+func Encrypt(plainText []byte, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plainText))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plainText)
+
+	encryptedDataStr := base64.StdEncoding.EncodeToString(ciphertext)
+
+	return encryptedDataStr, nil
+}
+
+func Decrypt(encryptedData string, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	cipherText, err := base64.StdEncoding.DecodeString(encryptedData)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(cipherText, cipherText)
+
+	return cipherText, nil
 }
