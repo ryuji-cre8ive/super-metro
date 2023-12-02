1bea0b697d98fd2214d789cfeb7f7cd7a44be3f2
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-12-03 00:51:18 +0800

    Add encryption tests for password and text encryption

--- backend/internal/utils/encrypt_test.go
+++ backend/internal/utils/encrypt_test.go
@@ -1,37 +1,98 @@
 package utils
 
 import (
+	"crypto/aes"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPasswordEncrypt(t *testing.T) {
-	password := "testPassword"
-	hashedPassword, err := PasswordEncrypt(password)
-	if err != nil {
-		t.Errorf("PasswordEncrypt failed with error: %v", err)
+	type input struct {
+		text []byte
+		key  []byte
 	}
 
-	err = CheckHashPassword(hashedPassword, password)
-	if err != nil {
-		t.Errorf("CheckHashPassword failed with error: %v", err)
+	tests := map[string]struct {
+		input input
+		want  error
+	}{
+		"success": {
+			input: input{
+				text: []byte("testPassword"),
+				key:  []byte("secretsecretsecretsecret"),
+			},
+			want: nil,
+		},
+		"failed: wrong-key": {
+			input: input{
+				text: []byte("testPassword"),
+				key:  []byte("secret"),
+			},
+			want: aes.KeySizeError(6),
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			_, err := Encrypt(tt.input.text, tt.input.key)
+			assert.Equal(t, tt.want, err)
+		})
 	}
 }
 
 func TestEncryptDecrypt(t *testing.T) {
-	key := []byte("secretsecretsecretsecret")
-	plainText := []byte("this is some text to encrypt")
-
-	encrypted, err := Encrypt(plainText, key)
-	if err != nil {
-		t.Errorf("Encrypt failed with error: %v", err)
+	type input struct {
+		plainText []byte
+		key       []byte
 	}
 
-	decrypted, err := Decrypt(encrypted, key)
-	if err != nil {
-		t.Errorf("Decrypt failed with error: %v", err)
+	tests := map[string]struct {
+		input input
+		want  string
+		err   error
+	}{
+		"success": {
+			input: input{
+				plainText: []byte("this is some text to encrypt"),
+				key:       []byte("secretsecretsecretsecret"),
+			},
+			want: "this is some text to encrypt",
+			err:  nil,
+		},
+		"failed: wrong-key": {
+			input: input{
+				plainText: []byte("this is some text to encrypt"),
+				key:       []byte("secret"),
+			},
+			want: "",
+			err:  aes.KeySizeError(6),
+		},
+		"failed: empty-text": {
+			input: input{
+				plainText: []byte(""),
+				key:       []byte("secretsecretsecretsecret"),
+			},
+			want: "",
+			err:  nil,
+		},
 	}
 
-	if string(decrypted) != string(plainText) {
-		t.Errorf("Decrypt text doesn't match original text")
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			encrypted, err := Encrypt(tt.input.plainText, tt.input.key)
+			if err != tt.err {
+				t.Errorf("Encrypt failed with error: %v", err)
+			}
+
+			decrypted, err := Decrypt(encrypted, tt.input.key)
+			if err != tt.err {
+				t.Errorf("Decrypt failed with error: %v", err)
+			}
+
+			if string(decrypted) != tt.want {
+				t.Errorf("Decrypt text doesn't match original text")
+			}
+		})
 	}
 }
