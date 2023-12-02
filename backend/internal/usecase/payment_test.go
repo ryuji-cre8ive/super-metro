f82ac05c3713a319e27cd9ccd17a3187cfb2f0db
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-12-02 23:05:18 +0800

    Add payment use case tests for delete and get methods

--- backend/internal/usecase/payment_test.go
+++ backend/internal/usecase/payment_test.go
@@ -2,8 +2,10 @@ package usecase
 
 import (
 	"testing"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/ryuji-cre8ive/super-metro/internal/domain"
 	"github.com/ryuji-cre8ive/super-metro/internal/stores"
 	mock "github.com/ryuji-cre8ive/super-metro/internal/stores/mock"
 	utilMock "github.com/ryuji-cre8ive/super-metro/internal/utils/mock"
@@ -120,6 +122,125 @@ func TestPaymentUsecase_Add(t *testing.T) {
 
 			err := u.Add(tt.input.ctx, tt.input.userId, tt.input.cardNumber, tt.input.expiryDate, tt.input.cvv)
 			assert.Equal(t, tt.want, err)
+		})
+	}
+}
+
+func TestPaymentUsecase_Delete(t *testing.T) {
+	type input struct {
+		ctx    echo.Context
+		userId string
+	}
+
+	wantedError := xerrors.New("error")
+
+	tests := map[string]struct {
+		input    input
+		want     error
+		mockFunc func(m *mock.MockPaymentStore)
+	}{
+		"success": {
+			input: input{
+				ctx:    nil,
+				userId: "6d1c3e1b-d068-431f-b188-a436ac54ce52",
+			},
+			want: nil,
+			mockFunc: func(m *mock.MockPaymentStore) {
+				m.EXPECT().Delete(gomock.Any()).Return(nil).Times(1)
+			},
+		},
+		"failed: standard-error": {
+			input: input{
+				ctx:    nil,
+				userId: "6d1c3e1b-d068-431f-b188-a436ac54ce52",
+			},
+			want: wantedError,
+			mockFunc: func(m *mock.MockPaymentStore) {
+				m.EXPECT().Delete(gomock.Any()).Return(wantedError)
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mock.NewMockPaymentStore(ctrl)
+			tt.mockFunc(store)
+
+			u := &paymentUsecase{
+				stores: &stores.Stores{
+					Payment: store,
+				},
+			}
+
+			err := u.Delete(tt.input.ctx, tt.input.userId)
+			assert.Equal(t, tt.want, err)
+		})
+	}
+}
+
+func TestPaymentUsecase_Get(t *testing.T) {
+	type input struct {
+		ctx    echo.Context
+		userId string
+	}
+
+	tests := map[string]struct {
+		input    input
+		want     *domain.Payment
+		wantErr  error
+		mockFunc func(m *mock.MockPaymentStore)
+	}{
+		"success": {
+			input: input{
+				ctx:    nil,
+				userId: "6d1c3e1b-d068-431f-b188-a436ac54ce52",
+			},
+			want: &domain.Payment{
+				UserID:     "6d1c3e1b-d068-431f-b188-a436ac54ce52",
+				CardNumber: "1234567890123456",
+				ExpiryDate: "1234",
+				CVV:        "123",
+				CreatedAt:  time.Date(2023, time.December, 2, 23, 3, 51, 115734000, time.Local),
+				UpdatedAt:  time.Date(2023, time.December, 2, 23, 3, 51, 115734000, time.Local),
+			},
+			wantErr: nil,
+			mockFunc: func(m *mock.MockPaymentStore) {
+				m.EXPECT().Get("6d1c3e1b-d068-431f-b188-a436ac54ce52").Return(&domain.Payment{
+					UserID:     "6d1c3e1b-d068-431f-b188-a436ac54ce52",
+					CardNumber: "1234567890123456",
+					ExpiryDate: "1234",
+					CVV:        "123",
+					CreatedAt:  time.Date(2023, time.December, 2, 23, 3, 51, 115734000, time.Local),
+					UpdatedAt:  time.Date(2023, time.December, 2, 23, 3, 51, 115734000, time.Local),
+				}, nil)
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mock.NewMockPaymentStore(ctrl)
+			tt.mockFunc(store)
+
+			u := &paymentUsecase{
+				stores: &stores.Stores{
+					Payment: store,
+				},
+			}
+
+			got, err := u.Get(tt.input.ctx, tt.input.userId)
+			if err != nil {
+				assert.Equal(t, tt.wantErr, err)
+			} else {
+				assert.Equal(t, tt.want, got)
+			}
+
 		})
 	}
 }
