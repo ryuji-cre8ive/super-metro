d2fea2d8259ce9aac03121617535cf3f24f583bf
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-12-03 21:52:30 +0800

    Add authorization token to axiosConfig

--- frontend/src/api/axiosConfig.tsx
+++ frontend/src/api/axiosConfig.tsx
@@ -1,11 +1,14 @@
 "use client";
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
+const token = window.localStorage.getItem("session_token");
+
 const axiosConfig: AxiosRequestConfig = {
   baseURL: "http://localhost:8080/api/v1",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
   },
 };
 
