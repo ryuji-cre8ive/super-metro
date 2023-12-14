c2fef92a515a4d9278d66d8031cf2b7578fbe27b
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-12-14 22:15:09 +0800

    Fix formatting issues in code

--- frontend/src/components/SnackBar.tsx
+++ frontend/src/components/SnackBar.tsx
@@ -2,11 +2,12 @@ import Snackbar from "@mui/material/Snackbar";
 import React from "react";
 import MuiAlert, { AlertProps, AlertColor } from "@mui/material/Alert";
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
-  function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  }
-);
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 type CustomSnackBarProps = {
   open: boolean;
